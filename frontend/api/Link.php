fa1f0d8a7f32665deb62be5ca4552ba8221ff9a9
iQs Solution <32163698+definaru@users.noreply.github.com>
2019-01-06 21:03:28 +0300

    Inc. Defina

---
+++ frontend/api/Link.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\api;
+
+class Link extends \yii\base\Module
+{
+
+    
+    public $controllerNamespace = 'app\api\controllers';
+    public $layout = '/api';
+
+    
+    
+    public function init()
+    {
+        parent::init();
+    }
+}
